908b66687dae067edadd311cba3adaefb501b272
benw000 <benjamin.winstanley.007@gmail.com>
2024-01-26 12:26:16 +0000

    first commit

---
+++ mymodule.py
@@ -0,0 +1,6 @@
+# Python 'module' is a .py file containing variables and functions
+# These might be called upon by the top-level 'script'
+mymodulevariable = 2.7
+def mymodulefunction(a,b):
+    c = a*b
+    return c
